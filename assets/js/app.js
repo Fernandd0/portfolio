391ca35341c4e583d47975977efb9f88563718bd
Fernando Solorzano <mr.fernandd.to@gmail.com>
2022-12-12 01:27:22 -0500

    30%

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
-const navToggle = document.querySelector("nav-toggle")
-const navList = document.querySelector("nav-list")
+// Main - Portada
+const header = document.querySelector("header");
+window.addEventListener("scroll", function() {
+    header.classList.toggle ("sticky", this.window.scrollY > 120);
+});
+
+/*
+const navToggle = document.querySelector("nav-toggle");
+const navList = document.querySelector("nav-list");
 
 navToggle.addEventListener("click", ()=> {
     navList.classList.toggle("nav-list-visible");
 });
+*/
