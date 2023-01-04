731aeb6a3d9ce6ddaea6488ef2d8b225f61ef785
Fernando S <fernandd.to.dev@gmail.com>
2023-01-04 00:42:47 -0500

    36%

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,3 @@
-// Main - Portada
-const header = document.querySelector("header");
-window.addEventListener("scroll", function() {
-    header.classList.toggle ("sticky", this.window.scrollY > 120);
-});
-
 /*
 const navToggle = document.querySelector("nav-toggle");
 const navList = document.querySelector("nav-list");
