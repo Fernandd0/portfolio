e58c882b158d13d809c202f86a693a29c96536ca
Fernando Solorzano <mr.fernandd.to@gmail.com>
2023-01-08 23:35:25 -0500

    Nav: responsive ✔️

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,25 @@
-/*
-const navToggle = document.querySelector("nav-toggle");
-const navList = document.querySelector("nav-list");
+// Nav hamburgerburger selections
+const burger = document.querySelector(".nav-toggle");
+const ul = document.querySelector(".nav-list");
+const nav = document.querySelector("#nav");
 
-navToggle.addEventListener("click", ()=> {
-    navList.classList.toggle("nav-list-visible");
+// Select nav links and hamburguer
+const navLink = document.querySelectorAll(".nav-link");
+const navtheme = document.querySelectorAll(".toggle-theme");
+
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
 });
-*/
+
+// Close hamburger menu when a link or ico-color is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+navtheme.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
