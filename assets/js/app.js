e2a6928a1eb41976529e24b0f625837dec4ccf66
Fernando Solorzano <mr.fernandd.to@gmail.com>
2023-01-13 04:09:57 -0500

    About Me: button,text, img, div ✔️

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,6 +24,7 @@ navtheme.forEach((link) =>
   })
 );
 
+
 //Color Mood
 let toggle = document.getElementById("toggle");
 let label = document.getElementById("label_toggle");
@@ -31,9 +32,9 @@ toggle.addEventListener("change", (event) => {
   let estado = event.target.checked;
   document.body.classList.toggle("dark-mood");
   if (estado) {
-    label.innerHTML = '<img src="assets/ico/ico-sun.png" alt="sun mood" class="ico-mood ico-mood-sun"/>';
+    label.innerHTML = '<img src="assets/ico/ico-sun.png" alt="sun mood" class="ico-mood ico-mood-sun" width="30" height="30"/>';
   }else {
-    label.innerHTML = '<img src="assets/ico/ico-moon.png" alt="dark mood" class="ico-mood"/>';
+    label.innerHTML = '<img src="assets/ico/ico-moon.png" alt="dark mood" class="ico-mood" width="30" height="30"/>';
   }
 });
 //Color Mood 2 for Responsive
@@ -43,11 +44,28 @@ toggle2.addEventListener("change", (event) => {
   let estado2 = event.target.checked;
   document.body.classList.toggle("dark-mood");
   if (estado2) {
-    label2.innerHTML = '<img src="assets/ico/ico-sun.png" alt="sun mood" class="ico-mood ico-mood-sun"/>';
+    label2.innerHTML = '<img src="assets/ico/ico-sun.png" alt="sun mood" class="ico-mood ico-mood-sun" width="30" height="30"/>';
   }else {
-    label2.innerHTML = '<img src="assets/ico/ico-moon.png" alt="dark mood" class="ico-mood"/>';
+    label2.innerHTML = '<img src="assets/ico/ico-moon.png" alt="dark mood" class="ico-mood" width="30" height="30"/>';
   }
 });
+
+
+//More About Me (text)
+let btn_text = document.getElementById("btn_text");
+let more_text = document.getElementById("more_text");
+
+btn_text.addEventListener("click", changeStyle);
+function changeStyle(){
+  more_text.classList.toggle("c-show-text");
+
+  if(more_text.classList.contains("c-show-text")){
+    btn_text.innerHTML = "Less About Me";
+  } else {
+    btn_text.innerHTML = "More About Me";
+  };
+};
+
 
 // Scroll to top selection
 const scrollUp = document.querySelector("#scroll-up");
