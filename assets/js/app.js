a6efa24e1d00629154a85bd302bb571ddf0fd34e
Fernando S <fernandd.to.dev@gmail.com>
2023-01-16 22:07:10 -0500

    Projects: progress carrusel-1

--- assets/js/app.js
+++ assets/js/app.js
@@ -100,3 +100,32 @@ function inicio() {
   setInterval(change, 3000);
 }
 window.onload=inicio;
+
+
+//Carrusel Projects
+const grande    = document.querySelector('.projects-container')
+const punto     = document.querySelectorAll('.projects-nav-punto')
+
+// Recorrer TODOS los punto
+punto.forEach( ( cadaPunto , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * - 33.3
+
+        // MOVEMOS el grand
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+
+    })
+})
