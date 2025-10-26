d109937d1eed3eef3cd09cf7fe357ab85e8fbc72
Fernando <mr.fernanddo@gmail.com>
2025-10-25 20:42:14 -0500

    refactor: remove ThanksSection component and inline its content in thanks page

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,20 +1,16 @@
 export const Footer = () => {
-  const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear()
   return (
     <footer>
       <div className="c_container c-footer-text">
-        <p className="footer-text-1">&copy; Copyright {currentYear}</p>
+        <p className="footer-text-1">&copy; {currentYear} — All rights reserved.</p>
         <p className="footer-text-2">
-          By{" "}
-          <a
-            href="https://github.com/Fernandd0"
-            target="_blank"
-            className="footer-link"
-          >
+          Built by{' '}
+          <a href="https://github.com/Fernandd0" target="_blank" className="footer-link">
             Fernando Solorzano
           </a>
         </p>
       </div>
     </footer>
-  );
-};
+  )
+}
